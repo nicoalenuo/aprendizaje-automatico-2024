f034a4f934d7321828cc310282e79c5aabf2ddc4
nicoalenuo <nicoalenuo@gmail.com>
2024-09-21 20:44:08 -0300

    finalizada entrega 2

--- 2 - Naive Bayes/funciones.py
+++ 2 - Naive Bayes/funciones.py
@@ -17,7 +17,7 @@ class NaiveBayesAIDS:
         Parametros
         ----------
         m: tamaño equivalente de muestra
-        valores_posibles: map {atributo: [valores posibles]} con los valores que puede tomar cada atributo
+        valores_posibles: diccionario con los valores posibles que puede tomar cada atributo
         '''
         self.m = m
         self.valores_posibles = valores_posibles
@@ -27,6 +27,9 @@ class NaiveBayesAIDS:
     def fit(self, X, Y):
         '''
         Aplica el algoritmo de Naive Bayes para entrenar el modelo
+        Guarda el logaritmo de las probabilidades de cada valor de cada atributo para cada clase
+        para facilitar el cálculo de la probabilidad de una entrada en el método predict
+        y evitar errores al multiplicar valores muy pequeños
         
         para evitar eventos con probabilidad 0,
         se aplica la m-estimacion: P(X|Y) = (e + m*p) / (m + n)
@@ -60,35 +63,6 @@ class NaiveBayesAIDS:
                 e = len(X[(X[cat] == val) & (Y == 1)])
                 self.probabilidades_1[cat][val] = (e + self.m * p) / (self.m + self.totales_1)
 
-    def __predict_entrada(self, X):
-
-        resultado_0 = math.log(self.totales_0 / self.total_entradas)
-        resultado_1 = math.log(self.totales_1 / self.total_entradas)
-
-        for cat in self.valores_posibles.keys():
-            resultado_0 += math.log(self.probabilidades_0[cat][X[cat]])
-            resultado_1 += math.log(self.probabilidades_1[cat][X[cat]])
-        
-        return 0 if resultado_0 > resultado_1 else 1
-
-    
-    def predict(self, X):
-        '''
-        Predice la clase de cada entrada en X
-        Utiliza el logaritmo de las probabilidades de cada valor de cada atributo para cada clase
-        para facilitar el cálculo de la probabilidad de una entrada
-        y evitar errores al multiplicar valores muy pequeños
-
-        Parametros
-        ----------
-        X: DataFrame con los datos a predecir
-        '''
-        if (not self.probabilidades_0 or not self.probabilidades_1):
-            raise Exception("El modelo no ha sido entrenado")
-
-        Y_predicho = [self.__predict_entrada(X.iloc[i]) for i in range(len(X))]
-        return Y_predicho
-    
     def __predict_proba_entrada(self, X):
         resultado_0 = self.totales_0 / self.total_entradas
         resultado_1 = self.totales_1 / self.total_entradas
@@ -101,14 +75,6 @@ class NaiveBayesAIDS:
         return [resultado_0 / norma, resultado_1 / norma]
 
     def predict_proba(self, X):
-        '''
-        Calcula la probabilidad de cada clase para cada entrada en X
-
-        Parametros
-        ----------
-        X: DataFrame con los datos a predecir
-        
-        '''
         if (not self.probabilidades_0 or not self.probabilidades_1):
             raise Exception("El modelo no ha sido entrenado")
 
@@ -116,11 +82,47 @@ class NaiveBayesAIDS:
         
         return Y_probs_predicho
 
+    def __predict_proba_log_entrada(self, X):
+        resultado_0 = math.log(self.totales_0 / self.total_entradas)
+        resultado_1 = math.log(self.totales_1 / self.total_entradas)
+
+        for cat in self.valores_posibles.keys():
+            resultado_0 += math.log(self.probabilidades_0[cat][X[cat]])
+            resultado_1 += math.log(self.probabilidades_1[cat][X[cat]])
+        
+        return [resultado_0, resultado_1]
+    
+    def predict_proba_log(self, X):
+        if (not self.probabilidades_0 or not self.probabilidades_1):
+            raise Exception("El modelo no ha sido entrenado")
+
+        Y_probs_predicho = [self.__predict_proba_log_entrada(X.iloc[i]) for i in range(len(X))]
+        
+        return Y_probs_predicho
+
+    def __predict_entrada(self, X):
+
+        resultado_0 = math.log(self.totales_0 / self.total_entradas)
+        resultado_1 = math.log(self.totales_1 / self.total_entradas)
+
+        for cat in self.valores_posibles.keys():
+            resultado_0 += math.log(self.probabilidades_0[cat][X[cat]])
+            resultado_1 += math.log(self.probabilidades_1[cat][X[cat]])
+        
+        return 0 if resultado_0 > resultado_1 else 1
+
+    def predict(self, X):
+        if (not self.probabilidades_0 or not self.probabilidades_1):
+            raise Exception("El modelo no ha sido entrenado")
+
+        Y_predicho = [self.__predict_entrada(X.iloc[i]) for i in range(len(X))]
+        return Y_predicho
+
     def get_params(self, deep=True):
         return {
             'm': self.m,
             'valores_posibles': self.valores_posibles
-        }
+            }
 
 
     def set_params(self, **params):
@@ -133,10 +135,6 @@ class NaiveBayesAIDS:
         return self
 
 def validacion_cruzada(X_train, Y_train, m, atributos_a_categorizar, valores_posibles, splits):
-    '''
-    Aplica validación cruzada para evaluar el modelo
-    Para esto, usa StratifiedKFold para que la distribución de clases sea similar en cada partición
-    '''
     skf = StratifiedKFold(n_splits = splits)
 
     scores_accuracy = []
@@ -146,10 +144,9 @@ def validacion_cruzada(X_train, Y_train, m, atributos_a_categorizar, valores_pos
     
     for train_index, val_index in skf.split(X_train, Y_train):
         
-        X_train_cv, X_val_cv = X_train.iloc[train_index], X_train.iloc[val_index]
-        y_train_cv, y_val_cv = Y_train.iloc[train_index], Y_train.iloc[val_index]
+        X_train_cv, X_val_cv= X_train.iloc[train_index], X_train.iloc[val_index]
+        y_train_cv, y_val_cv= Y_train.iloc[train_index], Y_train.iloc[val_index]
 
-        # Se generan copias para evitar modificar los datos originales
         X_train_copy = X_train_cv.copy()
         X_val_copy = X_val_cv.copy()
         
@@ -177,24 +174,20 @@ def validacion_cruzada(X_train, Y_train, m, atributos_a_categorizar, valores_pos
     
     return np.mean(scores_accuracy), np.mean(scores_precision), np.mean(scores_recall), np.mean(scores_f1)
 
-
-
 def seleccionar_subconjunto_a_eliminar(X_train, Y_train, m, potenciales_atributos_a_dropear, valores_posibles, atributos_a_categorizar):
-    '''
-    Aplica validación cruzada para seleccionar el subconjunto de atributos a eliminar
-    Para esto, itera sobre todos los subconjuntos posibles de atributos a eliminar y selecciona el que maximiza el F1    
-    '''
     accuracy_max = -1
     f1_max = -1
     subset_optimo = []
 
     for r in range(len(potenciales_atributos_a_dropear) + 1):
         for subset in itertools.combinations(potenciales_atributos_a_dropear, r):
-
             valores_posibles_aux = valores_posibles.copy()
-            atributos_a_categorizar_aux = atributos_a_categorizar.copy()
+            
             for key in list(subset):
                 valores_posibles_aux.pop(key)
+
+            atributos_a_categorizar_aux = atributos_a_categorizar.copy()
+            for key in list(subset):
                 if key in atributos_a_categorizar_aux:
                     atributos_a_categorizar_aux.remove(key)
 
@@ -249,22 +242,52 @@ def curva_precision_recall(modelo, X, Y):
 
     thresholds = np.append(thresholds, 1)
 
-    plt.figure(figsize=(8, 6))
+    # Primera gráfica
+    plt.figure(figsize=(7, 5))
     plt.plot(thresholds, precision, label='Precision', color='blue')
     plt.plot(thresholds, recall, label='Recall', color='green')
 
-    plt.xlabel('Threshold')
+    plt.xlabel('Umbral')
     plt.ylabel('Precision / Recall')
     plt.title('Curva Precision-Recall')
     plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0]) 
+    plt.ylim([0.0, 1.0])
+    plt.legend()
+    plt.grid(True)
+
+    # Diferencias para encontrar el umbral óptimo
+    diferencias = np.abs(precision - recall)
+    indice_minimo = np.argmin(diferencias)
+    umbral_mejor = thresholds[indice_minimo]
+
+    print(f'Umbral donde se da el cruce: {umbral_mejor}')
+
+
+def curva_precision_recall_log(modelo, X, Y):
+    prob_log = modelo.predict_proba_log(X)
+
+    # Me quedo con clase 0
+    prob_log_0 = [item[0] for item in prob_log]
+
+    precision_log, recall_log, thresholds_log = precision_recall_curve(Y, prob_log_0, pos_label=0)
+
+    precision_log = precision_log[:-1]
+    recall_log = recall_log[:-1]
+
+    plt.figure(figsize=(7, 5))
+    plt.plot(thresholds_log, precision_log, label='Precision', color='blue')
+    plt.plot(thresholds_log, recall_log, label='Recall', color='green')
+
+    plt.xlabel('Umbral')
+    plt.ylabel('Precision / Recall')
+    plt.title('Curva Precision-Recall (probabilidad logarítimica)')
     plt.legend()
     plt.grid(True)
     plt.show()
 
-    diferencias = np.abs(precision - recall)
+    diferencias = np.abs(precision_log - recall_log)
     indice_minimo = np.argmin(diferencias)
-    umbral_mejor = thresholds[indice_minimo]
+    umbral_mejor = thresholds_log[indice_minimo]
 
     print(f'Umbral donde se da el cruce: {umbral_mejor}')
 
